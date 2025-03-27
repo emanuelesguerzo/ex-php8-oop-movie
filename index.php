0dcce86d1b75213751ea1a5b9a43035bafd67ccb
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2025-03-27 17:11:08 +0100

    feat: added more genres

--- index.php
+++ index.php
@@ -42,12 +42,20 @@ class Genre {
     }
 }
 
+// Genres
 $drama = new Genre("Drammatico", "Film emotivamente intensi");
 $thriller = new Genre("Thriller", "Film con suspense e colpi di scena");
+$adventure = new Genre("Avventura", "Film ricchi di esplorazioni, viaggi e sfide eroiche");
+$action = new Genre("Azione", "Film dinamici con combattimenti, inseguimenti e adrenalina");
+$historical = new Genre("Storico", "Film ambientati in epoche passate, spesso basati su eventi reali");
+$war = new Genre("Guerra", "Film che raccontano conflitti armati e le esperienze dei soldati");
+$suspense = new Genre("Suspense", "Film che creano tensione e mistero, tenendo lo spettatore col fiato sospeso");
 
-$lastSamuraiGenres = [$drama, $thriller];
-$imitationGameGenres = [$drama, $thriller];
+// Genres Arrays
+$lastSamuraiGenres = [$adventure, $drama, $historical, $action];
+$imitationGameGenres = [$historical, $thriller, $war, $action, $suspense];
 
+// Movies
 $theLastSamurai = new Movie("The Last Samurai", "Edward Zwick", 2003, 150, $lastSamuraiGenres);
 $theImitationGame = new Movie("The Imitation Game", "Morten Tyldum", 2014, 115, $imitationGameGenres);
 
